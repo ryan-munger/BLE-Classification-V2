f07ab572973f40312253f40c2fe2dd94c5ac2fec
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-11-06 09:40:26 -0500

    added comments

--- sklearn-env/src/cleaningdata.py
+++ sklearn-env/src/cleaningdata.py
@@ -6,27 +6,31 @@ import argparse
 import os.path
 import sys
 
+# cleans the data
+def cleaning_data(in_csv):
+    df = pd.read_csv(in_csv)
+    df.head()
+
+# script requires command line argument --csv "file path"
 def command_line_args():
+    # parses argument input
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv', metavar='<input csv file>',
                         help='csv file to parse', required=True)
     args = parser.parse_args()
     return args
 
-def cleaning_data(in_csv):
-    df = pd.read_csv(in_csv)
-    df.head()
-
-
+# main function
 def main():
-    args = command_line_args()
+    args = command_line_args()  #grab command line arguments
 
+    # does the path exist?
     if not os.path.exists(args.csv):
         print('Input csv file "{}" does not exist'.format(args.csv),
               file=sys.stderr)
         sys.exit(-1)
 
-    cleaning_data(args.csv)
+    cleaning_data(args.csv) # call cleaning_data to start cleansing
 
 # start script
 if __name__ == "__main__":
