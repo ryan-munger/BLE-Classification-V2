bc3ba1722fabd96d16b572667f9fd40f7d0b1ec3
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-11-24 18:42:29 -0500

    successfully cleaned all of the data. Fixed issues with combining csv and data types. Ran into another issue with model only using numerical values... how to resolve?

--- sklearn-env/src/temp.py
+++ sklearn-env/src/temp.py
@@ -2,6 +2,7 @@
 
 # --imports--
 import pandas as pd
+import numpy as np
 import argparse
 import os.path
 import sys
@@ -10,59 +11,41 @@ import sys
 def cleaning_data(in_csv):
     try:
         # read the csv in to pandas using ISO encoding
-        df = pd.read_csv(in_csv, encoding='ISO-8859-1', dtype='string', header=0)
-    except Exception as e:
-        print(f"Was unable to open the file... Error: {e}")
+        df = pd.read_csv(in_csv, encoding='ISO-8859-1', low_memory=False)
+    except:
+        print("Was unable to open the file...")
         sys.exit(-1)
 
     # these are the columns we are expecting in the csv file
-    expected_columns = {
-        'No.': 'int64',
-        'Source': 'string',
-        'Destination': 'string',
-        'Protocol': 'string',
-        'Length': 'int64',
-        'Timestamp': 'string',
-        'RSSI': 'int64',
-        'Channel Index': 'int64',
-        'Advertising Address': 'string',
-        'Company ID': 'string',
-        'Packet counter': 'int64',
-        'Protocol version': 'int64',
-        'UUID 16': 'string',
-        'Entry': 'string',
-        'Device Name': 'string',
-        'Power Level (dBm)': 'int64',
-        'Info': 'string',
-        'Label': 'int64'
-    }
+    expected_columns = [
+        'No.', 'Source', 'Destination', 'Protocol', 'Length', 'Timestamp', 
+        'RSSI', 'Channel Index', 'Advertising Address', 'Company ID', 
+        'Packet counter', 'Protocol version', 'UUID 16', 'Entry', 
+        'Device Name', 'Power Level (dBm)', 'Info', 'Label'
+    ]
 
-    # filling columns NaN values
-    df['No.'] = df['No.'].fillna("-1")
+    df = df[expected_columns]   # set to make sure
+
+    # fix types
+    
+    # fill NaN with information
     df['Source'] = df['Source'].fillna("00:00:00:00:00:00")
     df['Destination'] = df['Destination'].fillna("broadcast")
     df['Protocol'] = df['Protocol'].fillna("Unknown")
-    df['Length'] = df['Length'].fillna("-1")
-    df['Timestamp'] = df['Timestamp'].fillna("-1.0")
-    df['RSSI'] = df['RSSI'].fillna("1")
-    df['Channel Index'] = df['Channel Index'].fillna("-1")
+    df['Length'] = df['Length'].fillna(-1)
+    df['Timestamp'] = df['Timestamp'].fillna(-1.0)
+    df['RSSI'] = df['RSSI'].fillna(1)
+    df['Channel Index'] = df['Channel Index'].fillna(-1)
     df['Advertising Address'] = df['Advertising Address'].fillna("00:00:00:00:00:00")
-    df['Company ID'] = df['Company ID'].fillna("Unknown")
-    df['Packet counter'] = df['Packet counter'].fillna("-1")
-    df['Protocol version'] = df['Protocol version'].fillna("-1")
+    df['Company ID'] = df['Company ID'].fillna(-1)
+    df['Packet counter'] = df['Packet counter'].fillna(-1)
+    df['Protocol version'] = df['Protocol version'].fillna(-1)
     df['UUID 16'] = df['UUID 16'].fillna("None")
     df['Entry'] = df['Entry'].fillna("None")
     df['Device Name'] = df['Device Name'].fillna("Unnamed")
-    df['Power Level (dBm)'] = df['Power Level (dBm)'].fillna("-255")
+    df['Power Level (dBm)'] = df['Power Level (dBm)'].fillna(-255)
     df['Info'] = df['Info'].fillna("Unknown")
-    df['Label'] = df['Label'].fillna("-1")
-
-    # cleaning and converting column types
-    df['RSSI'] = df['RSSI'].str.replace(' dBm', '').str.strip().astype(int)
-    for column, dtype in expected_columns.items():
-            if column not in ['RSSI']:
-                df[column] = df[column].str.replace(r'[,|-]', '', regex=True)
-                df[column] = df[column].astype(dtype)
+    df['Label'] = df['Label'].fillna(-1)
 
     # save the cleaned data to the script dir
     script_dir = os.path.dirname(os.path.abspath(__file__))
